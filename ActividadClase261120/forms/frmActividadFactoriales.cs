b1916c2db26a0cf21905d73668fbd0ee7238d6db
RICARDO VACA <ricardo.vaca@pucese.edu.ec>
2020-11-26 12:11:42 -0500

    Agregar archivos de proyecto.

---
+++ ActividadClase261120/forms/frmActividadFactoriales.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ActividadClase261120.forms
+{
+    public partial class frmActividadFactoriales : Form
+    {
+        public frmActividadFactoriales()
+        {
+            InitializeComponent();
+        }
+    }
+}
